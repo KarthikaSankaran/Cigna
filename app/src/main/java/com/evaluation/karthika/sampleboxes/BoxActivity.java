79b34c514a24ebf047208fb1d27a01ae07768b6e
Ramkumar Sakthivel (M42730) <ramkumar.sakthivel2@cigna.com>
2017-06-08 20:58:57 +0530

    with views

--- app/src/main/java/com/evaluation/karthika/sampleboxes/BoxActivity.java
+++ app/src/main/java/com/evaluation/karthika/sampleboxes/BoxActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
@@ -13,10 +14,29 @@ public class BoxActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        LinearLayout linear;
+        int count;
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         String message = intent.getStringExtra("BOX NUMBER");
+        count=Integer.parseInt(message);
         this.setTitle("Cigna - " + message);
-        setContentView(new CustomView(this,Integer.parseInt(message)));
+        LinearLayout picLL = new LinearLayout(this);
+        picLL.layout(0, 0, 100, 100);
+        picLL.setLayoutParams(new LinearLayout.LayoutParams(100, 100));
+        picLL.setOrientation(LinearLayout.HORIZONTAL);
+        //for(int i=0;i<count;i++){
+        View v=new View(this);
+        v.setBackgroundResource(R.drawable.box);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(300,300);
+        v.setLayoutParams(params);
+        setContentView(picLL);
+
+
+
+
+        //}
+
+        //setContentView(new CustomView(this,Integer.parseInt(message)));
     }
 }
