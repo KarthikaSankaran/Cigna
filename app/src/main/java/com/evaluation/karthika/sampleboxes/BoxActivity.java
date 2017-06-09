bc9eb3505d9e18806935f8aca7fda1f7579c4afd
KarthikaSankaran <KarthikaSankaran@users.noreply.github.com>
2017-06-09 08:48:04 +0530

    Update BoxActivity.java
    Working copy

--- app/src/main/java/com/evaluation/karthika/sampleboxes/BoxActivity.java
+++ app/src/main/java/com/evaluation/karthika/sampleboxes/BoxActivity.java
@@ -1,42 +1,32 @@
 package com.evaluation.karthika.sampleboxes;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 
 public class BoxActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         LinearLayout linear;
         int count;
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         String message = intent.getStringExtra("BOX NUMBER");
-        count=Integer.parseInt(message);
+        count = Integer.parseInt(message);
         this.setTitle("Cigna - " + message);
-        LinearLayout picLL = new LinearLayout(this);
-        picLL.layout(0, 0, 100, 100);
-        picLL.setLayoutParams(new LinearLayout.LayoutParams(100, 100));
-        picLL.setOrientation(LinearLayout.HORIZONTAL);
-        //for(int i=0;i<count;i++){
-        View v=new View(this);
-        v.setBackgroundResource(R.drawable.box);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(300,300);
-        v.setLayoutParams(params);
-        setContentView(picLL);
+        setContentView(R.layout.box_layout);
+        linear = (LinearLayout) findViewById(R.id.linear);
+        for (int i = 0; i < count; i++) {
+            View v = new View(this);
+            v.setBackgroundResource(R.drawable.box);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(200, 200);
+            params.setMargins(20, 20, 20, 20);
+            v.setLayoutParams(params);
+            linear.addView(v);
 
-
-
-
-        //}
-
-        //setContentView(new CustomView(this,Integer.parseInt(message)));
+        }
     }
 }
