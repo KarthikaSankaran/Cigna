3abd38f463ae63eb79c73c76d0a7acb977c4ef63
KarthikaSankaran <KarthikaSankaran@users.noreply.github.com>
2017-06-09 11:40:15 +0530

    Update BoxActivity.java
    Changed to gridlayout

--- app/src/main/java/com/evaluation/karthika/sampleboxes/BoxActivity.java
+++ app/src/main/java/com/evaluation/karthika/sampleboxes/BoxActivity.java
@@ -4,13 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.LinearLayout;
 
 public class BoxActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        LinearLayout linear;
+        GridLayout grid;
         int count;
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
@@ -18,14 +19,16 @@ public class BoxActivity extends AppCompatActivity {
         count = Integer.parseInt(message);
         this.setTitle("Cigna - " + message);
         setContentView(R.layout.box_layout);
-        linear = (LinearLayout) findViewById(R.id.linear);
+        grid = (GridLayout) findViewById(R.id.grid);
         for (int i = 0; i < count; i++) {
             View v = new View(this);
             v.setBackgroundResource(R.drawable.box);
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(200, 200);
+            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+            params.height=200;
+            params.width=200;
             params.setMargins(20, 20, 20, 20);
             v.setLayoutParams(params);
-            linear.addView(v);
+            grid.addView(v);
 
         }
     }
