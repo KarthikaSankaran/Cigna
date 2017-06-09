7a84f4328520bf234690b4626e7801ed4f313fe2
KarthikaSankaran <KarthikaSankaran@users.noreply.github.com>
2017-06-09 16:34:30 +0530

    Create BoxActivity.java
    Layout inflater added

--- app/src/main/java/com/evaluation/karthika/sampleboxes/BoxActivity.java
+++ app/src/main/java/com/evaluation/karthika/sampleboxes/BoxActivity.java
@@ -1,34 +1,33 @@
 package com.evaluation.karthika.sampleboxes;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.GridLayout;
-import android.widget.LinearLayout;
 
 public class BoxActivity extends AppCompatActivity {
+    GridLayout grid;
+    int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        GridLayout grid;
-        int count;
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.TAG);
+        String message = intent.getStringExtra(MainActivity.PUT_EXTRA);
         count = Integer.parseInt(message);
         this.setTitle("Cigna - " + message);
         setContentView(R.layout.box_layout);
         grid = (GridLayout) findViewById(R.id.grid);
         for (int i = 0; i < count; i++) {
-            View v = new View(this);
-            v.setBackgroundResource(R.drawable.box);
-            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
-            params.height=200;
-            params.width=200;
-            params.setMargins(20, 20, 20, 20);
-            v.setLayoutParams(params);
-            grid.addView(v);
+
+
+            LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService
+                    (Context.LAYOUT_INFLATER_SERVICE);
+            View view = inflater.inflate(R.layout.viewlayout, null);
+            grid.addView(view);
 
         }
     }
